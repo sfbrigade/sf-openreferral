b897b695d5019be1b6c3f78cedbb93ec40f0bcca
= <rsiegel827@gmail.com>
2015-06-06 16:01:38 -0700

    fixing syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
   resources :organizations
-  get "/pending", to: "organizations#pending" as: :pending_organizations
+  get "/pending", to: "organizations#pending", as: :pending_organizations
   root to: "organizations#index"
 end
