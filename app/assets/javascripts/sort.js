c1517c0b6ef48f65f128d6779ee7ce43de9cdd47
= <rsiegel827@gmail.com>
2015-06-06 14:30:27 -0700

    fixed semicolons

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -2,26 +2,28 @@
 
 //Sort all tables by header row
 $('.sortable th').click(function(){
-    var table = $(this).parents('table').eq(0)
-    var rows = table.find("tr:not(:has('th'))").toArray().sort(comparer($(this).index()))
-    this.asc = !this.asc
+
+
+    var $table = $(this).parents('table').eq(0);
+    var rows = $table.find("tr:not(:has('th'))").toArray().sort(comparer($(this).index()));
+    this.asc = !this.asc;
 
     if (!this.asc){
-        rows = rows.reverse()
+        rows = rows.reverse();
     }
 
     for (var i = 0; i < rows.length; i++){
-        table.append(rows[i])
+        $table.append(rows[i]);
     }
 })
 
 function comparer(index) {
     return function(a, b) {
-        var valA = getCellValue(a, index), valB = getCellValue(b, index)
-        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+        var valA = getCellValue(a, index), valB = getCellValue(b, index);
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB);
     }
 }
 
 function getCellValue(row, index){
-    return $(row).children('td').eq(index).html()
+    return $(row).children('td').eq(index).html();
 }
