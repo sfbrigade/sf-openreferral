b136722543a61c7e6072720907fddbf655e84357
Grayson Wright <wright.grayson@gmail.com>
2015-06-06 01:04:48 -0700

    Add organization seed data from ohana-api database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+organizations = YAML.load_file("db/seeds/organizations.yml")
+
+organizations.each do |organization_attributes|
+  Organization.create(organization_attributes)
+end
