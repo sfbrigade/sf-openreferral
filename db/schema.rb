5c31ce87ac733267759a64f3625948c0f5fffae5
Grayson Wright <wright.grayson@gmail.com>
2015-06-06 12:01:55 -0700

    Add status to organizations
    Default pending

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606061324) do
+ActiveRecord::Schema.define(version: 20150606190125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,9 @@ ActiveRecord::Schema.define(version: 20150606061324) do
     t.string   "phone"
     t.string   "email"
     t.string   "url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "status",      default: "pending", null: false
   end
 
 end
