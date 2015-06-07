a722971427d627bc9c5e51c3683a6708411eb97e
Josh Southern <josh@joshsouthern.ca>
2015-06-06 19:43:42 -0700

    fixed some styles resolved merge conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def title
-  	"SF Open Referral"
-  end
   before_action :authenticate_admin!
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
   resources :organizations
-<<<<<<< HEAD
 
-  # root to: "pages/show"
-=======
   get "/pending", to: "organizations#pending", as: :pending_organizations
-  root to: "organizations#index"
+  
+  # root to: "organizations#index"
+  
   get "/api", to: "api#all"
->>>>>>> e12556bf1c35b5330fc2bc92d7af4548bdd6b88b
 end
