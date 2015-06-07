3d64c94c3981d6142b0a73938703096cee651a49
= <rsiegel827@gmail.com>
2015-06-06 22:37:29 -0700

    Fixing some pjax stuff

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -1,6 +1,6 @@
 $(document).pjax('a.organization-link', '#organization-content', {push: false} );
 $(document).pjax('a.edit-link-pjax', '#organization-content', {push: false} );
 
-$(document).on('submit', 'form', function(event) {
+$(document).on('submit', '#edit-form form', function(event) {
     $.pjax.submit(event, '#organization-content', {push: false})
 });
