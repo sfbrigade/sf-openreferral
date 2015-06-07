e1f55b65fe901b6f7b34ff9ce7f5fcdf83ca2fdb
Josh Southern <josh@joshsouthern.ca>
2015-06-07 11:47:32 -0700

    search filtering

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -4,3 +4,23 @@ $(document).pjax('a.edit-link-pjax', '#organization-content', {push: false} );
 $(document).on('submit', '#edit-form form', function(event) {
     $.pjax.submit(event, '#organization-content', {push: false});
 });
+
+// retrieve organization list
+$('.organization-search input').change(function (){
+	var filter = $(this).val();
+	var regex  = new RegExp(filter, 'i');
+
+	$('.organization-listing a').each(function (index){
+		var matches = $(this).text().match(regex);
+
+		// matches
+		if (matches !== null){
+			$(this).parent().removeClass('hidden');
+		}
+
+		// does not match
+		else {
+			$(this).parent().addClass('hidden');
+		}
+	});
+});
