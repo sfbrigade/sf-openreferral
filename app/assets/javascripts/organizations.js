f2b12ca924ca534477f4430a11efaa89c99f99e2
= <rsiegel827@gmail.com>
2015-06-07 12:21:18 -0700

    Adding google maps

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -4,3 +4,9 @@ $(document).pjax('a.edit-link-pjax', '#organization-content', {push: false} );
 $(document).on('submit', '#edit-form form', function(event) {
     $.pjax.submit(event, '#organization-content', {push: false});
 });
+
+$('#organization-content').on('pjax:success', function() {
+    if (('.org-address').length >= 1 ){
+        displayMap($('.org-address').text());
+    }
+})
