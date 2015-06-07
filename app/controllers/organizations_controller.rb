2c4635ec3c5b1e1b8e8db05a59f07931d5ff4b6b
Josh Southern <josh@joshsouthern.ca>
2015-06-06 19:58:15 -0700

    reverted login change

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,4 @@
 class OrganizationsController < ApplicationController
-  before_action :authenticate_admin!
   before_action :set_organization, only: [:show, :edit, :update, :destroy]
 
   def index
