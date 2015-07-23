b3241b38d94aae24dd683fc2f9090c49edf3bb95
Becky Siegel <rsiegel@groupon.com>
2015-07-22 21:03:27 -0700

    Moving text input js to separate file, switching to jquery

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -66,36 +66,3 @@ function displayMap(address) {
     }
   });
 }
-
-//This makes the description field autosize to the size of the text content
-var observe;
-if (window.attachEvent) {
-  observe = function (element, event, handler) {
-    element.attachEvent('on'+event, handler);
-  };
-}
-else {
-  observe = function (element, event, handler) {
-    element.addEventListener(event, handler, false);
-  };
-}
-function initTextArea () {
-  var text = document.getElementById('organization_description');
-  function resize () {
-    text.style.height = 'auto';
-    text.style.height = text.scrollHeight+'px';
-  }
-  /* 0-timeout to get the already changed text */
-  function delayedResize () {
-    window.setTimeout(resize, 0);
-  }
-  observe(text, 'change',  resize);
-  observe(text, 'cut',     delayedResize);
-  observe(text, 'paste',   delayedResize);
-  observe(text, 'drop',    delayedResize);
-  observe(text, 'keydown', delayedResize);
-
-  text.focus();
-  text.select();
-  resize();
-}
---
+++ app/assets/javascripts/textinputs.js
@@ -0,0 +1,22 @@
+//This makes the description field autosize to the size of the text content
+var observe = function (element, event, handler) {
+  $(window).bind(event, handler);
+};
+
+function initTextArea () {
+  var $text = $('#organization_description');
+  function resize () {
+    $text.height( 'auto' );
+    $text.height( $('textarea')[0].scrollHeight );
+  }
+  /* 0-timeout to get the already changed text */
+  function delayedResize () {
+    window.setTimeout(resize, 0);
+  }
+  observe($text, 'change',  resize);
+  observe($text, 'cut',     delayedResize);
+  observe($text, 'paste',   delayedResize);
+  observe($text, 'drop',    delayedResize);
+  observe($text, 'keydown', delayedResize);
+  resize();
+}
