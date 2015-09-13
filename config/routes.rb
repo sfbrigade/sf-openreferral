288b750c17269e677c66cb6b59e6f01cc7f57917
Grayson Wright <wright.grayson@gmail.com>
2015-09-13 14:26:19 -0700

    Remove markdown exporting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
 
-  get "/api/exportformats", to: "api#export_formats"
-  get "/api/export/all", to: "api#export_all"
-  get "/api/export/:id", to: "api#export_organization", as: :api_export
   get "/pending", to: "organizations#pending", as: :pending_organizations
 
   get "/organizations/sidebar", to: "organizations#sidebar"
